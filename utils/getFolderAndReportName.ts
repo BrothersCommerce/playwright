95fb6c8f1b39a9edf389f64a607b33a156a69114
Daniel Gustavsson <daniel.gustavsson@brothers.se>
2024-12-10 16:15:16 +0100

    added sendgrid and save file to service

---
+++ utils/getFolderAndReportName.ts
@@ -0,0 +1,15 @@
+export const getFolderAndReportName = ({ testTarget, saleStatus }: { testTarget: string, saleStatus: string }) => {
+    const timestamp = new Date();
+    const year = timestamp.getUTCFullYear();
+    const month = timestamp.getUTCMonth() + 1;
+    const date = timestamp.getUTCDate();
+    const folderName = `${year}-${month}-${date}`;
+
+    let excelReportName = testTarget.replace("%", "procent").replace(".", "");
+
+    if (saleStatus) {
+        excelReportName = `${excelReportName}__${saleStatus}__`;
+    }
+
+    return { folderName, excelReportName };
+}
