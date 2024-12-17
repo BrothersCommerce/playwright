ad13a88e4a399d1ac28a7aac3e6ea8e241f16c9f
Daniel Gustavsson <daniel.gustavsson@brothers.se>
2024-12-17 15:17:54 +0100

    first test with interactive inputs

--- tests/brothers.se/selected-skus/selected-skus.spec.ts
+++ tests/brothers.se/selected-skus/selected-skus.spec.ts
@@ -21,7 +21,7 @@ const mitigatedErrors = getMitigatedErrors(`
 let { page, excelRows, testTarget, duplicatedRows, offlineProductPages, excelSLP, magentoData } = setupTestData((process.env.SALE_NAME ?? "unknownSale"));
 
 const getEnvInput = (env?: string): Promise<string[]> => new Promise((resolve) => resolve((env ?? "").trim().split(/\n/).filter(v => v.length > 0).map(v => v.trim())));
-console.log(process.env.SKUS);
+
 let skus;
 let salePrices;
 let slps;
@@ -41,11 +41,6 @@ test.afterAll(async () => {
 });
 
 test(`PLP: ${testTarget}`, async () => {
-    console.log("skus in PLP: ", process.env.SKUS);
-    skus = (await getEnvInput(process.env.SKUS)).filter(sku => !sku.split("")[0].match(/[a-z,A-Z]/g));
-    salePrices = (await getEnvInput(process.env.SALE_PRICES)).filter(sku => !sku.split("")[0].match(/[a-z,A-Z]/g)).map(v => v.replace(",", "").replace(" ", "")).map(v => +v);
-    slps = (await getEnvInput(process.env.SLP)).filter(sku => !sku.split("")[0].match(/[a-z,A-Z]/g)).map(v => v.replace(",", "").replace(" ", "")).map(v => +v);
-
     const data = await testPLP({ testTarget, skus, page, mitigatedErrors });
     offlineProductPages = data.offlineData;
     excelRows.push({ label: data.label, result: data.result });
